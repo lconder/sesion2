9b694246200fc9dba33f0bd1e144917c7f3de5af
Luis Conde <conderodriguez.luis@outlook.com>
2018-01-09 19:26:04 -0600

    sesion 2

---
+++ sesion2.playground/Contents.swift
@@ -0,0 +1,50 @@
+
+import Foundation
+
+var x = 4
+
+var y: Int = 6
+
+x / y
+
+x * y
+
+x + y
+
+x - y
+
+x % y
+
+var weight: Float = 65.675628917837983217
+
+var weight_: Double =  65.675628917837983217
+
+var pi = 3.1416
+
+var n = 3.85
+
+var r = 3
+
+pi * Double(r) * Double(r)
+
+
+floor(n)
+
+ceil(n)
+
+sqrt(n)
+
+pow(2, 5)
+
+
+var isPayDay = true
+
+
+var wallet = 200
+
+
+if isPayDay {
+    
+    wallet+=200
+}
+
--- sesion2/ViewController.swift
+++ sesion2/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var txtFieldDistance: UITextField!
+    
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet var labelResult: UILabel!
+    
+    let mileUnit = 1.6034
+    
+    let kilometerUnit = 0.621371
+    
+    var convertedValue = 0.0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        labelResult.text = "Ingresa un valor a calcular"
+        
     }
 
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        
+        let distanceToConvert = Double(txtFieldDistance.text!)
+        
+        let selectedIndex = segmentedControl.selectedSegmentIndex
+        
+        if selectedIndex==0 {
+            //de Millas a Km.
+            convertedValue = distanceToConvert! * mileUnit
+            print("\(distanceToConvert!) Millas = \(convertedValue) Km")
+            labelResult.text = "\(distanceToConvert!) Millas = \(convertedValue) Km"
+        }else{
+            //de Km a Millas
+            convertedValue = distanceToConvert! * kilometerUnit
+            print("\(distanceToConvert!) Km = \(convertedValue) Millas")
+            labelResult.text = "\(distanceToConvert!) Km = \(convertedValue) Millas"
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
